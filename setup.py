554050fd88f69a1f54acaecb69625a5678a1d1b0
Dave <dave.lee@goodoc.co.kr>
2019-03-10 17:05:02 +0900

    2019-02-26 test commit 1

---
+++ setup.py
@@ -0,0 +1,27 @@
+import cx_Freeze
+from cx_Freeze import setup, Executable
+import os
+import sys
+
+os.environ['TCL_LIBRARY'] = r'C:\Users\Goodoc\AppData\Local\Programs\Python\Python36-32\tcl\tcl8.6'
+os.environ['TK_LIBRARY'] = r'C:\Users\Goodoc\AppData\Local\Programs\Python\Python36-32\tcl\tk8.6'
+
+build_exe_options = dict(
+        packages = ["idna"],
+        includes = ["sys", "os", "datetime", "tkinter", "slackclient", "gspread", "oauth2client", "urllib3"],
+        include_files = ["cred.json", "tcl86t.dll", "tk86t.dll"]
+)
+
+#base = None
+#if sys.platform == "win32":
+#    base = "Win32GUI"
+
+setup(
+    name = "auto cs",
+    version = "1.0",
+    author="dave",
+    description = "auto cs",
+    options = {"build_exe": build_exe_options},
+    executables = [Executable("auto_cs_1.py", base = "Win32GUI")]
+)
+
